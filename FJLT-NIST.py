93ccb947c9945ac6b1ceccbcadf548dea9aeabeb
CapelliDiPaja <24519853+FireSterLine@users.noreply.github.com>
2019-10-21 23:48:46 +0800

    Add MNIST database, improved metric for error evaluation. Error is 99%.

--- FJLT-NIST.py
+++ FJLT-NIST.py
@@ -13,22 +13,45 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import time
 from sklearn.datasets import load_digits
+import sys
 
 
+def loadDigits():
+    n_clusters = 10
+    image_size = 8
+    image_pixels = image_size * image_size
 
+    digits = load_digits()
+    return digits.data, digits.target, n_clusters
+
+def loadMNIST():
+    n_clusters = 10
+    image_size = 28
+    image_pixels = image_size * image_size
+
+    mnist = np.loadtxt("data/mnist/mnist_test.csv", delimiter=",")/255
+    # ... mnist = np.loadtxt("data/mnist.csv", delimiter=",")/255
+
+    fac = 0.99 / 255
+    data   = np.asfarray(mnist[:, 1:]) * fac + 0.01
+    labels = np.asfarray(mnist[:, :1])
+
+    return data, labels, n_clusters
 
 def main():
-    digits = load_digits()
 
-    n = len(digits.images)
+    # data, labels, n_clusters = loadDigits()
+    data, labels, n_clusters = loadMNIST()
+
+    n = len(data)
 
     c = 1
     eps = 1
-    d = 64
+    d = data.shape[1]
     k = int(c*log(n)/(eps**2))+1
 
     print()
-    print("Fast Johnson-Lindenstrauss Transform applied to MNIST digit datase")
+    print("Fast Johnson-Lindenstrauss Transform applied to MNIST digit dataset")
     print("n = %d" % n)
     print("c = %d" % n)
     print("eps = %d" % eps)
@@ -36,24 +59,20 @@ def main():
     print()
     print("k = %d" % k)
 
-    # printDigits(digits)
 
-    phi = buildPhi(n,k,d)
-
+    # Apply FJLT
+    phi = FJLT_phi(n,k,d)
+    reduced_data = [phi.dot(entry) for entry in data]
 
     print()
     print("KMeans on the original data...")
-    array = []
-    for i in range(len(digits.images)):
-        array.append(matToList(digits.images[i]))
-    computeKMeans(array, 10, orig_data=digits)
+    evaluateKMeans(data, n_clusters, labels)
 
     print()
     print("FLJT + KMeans...")
-    array = []
-    for i in range(len(digits.images)):
-        array.append(phi.dot(matToList(digits.images[i])))
-    computeKMeans(array, 10, orig_data=digits)
+    evaluateKMeans(reduced_data, n_clusters, labels)
+
+    print()
 
 ### --- FJLT --- ###
 
@@ -66,14 +85,7 @@ def binaryDot(x,y):
             res += 1
     return res % 2
 
-def matToList(mat):
-    res = []
-    for i in mat:
-        for elem in i:
-            res.append(elem)
-    return np.array(res)
-
-def buildPhi(n, k, d):
+def FJLT_phi(n, k, d):
     # Note : assume the p in the article is 2
     q = min(1,(log(n)**2)/d)
     P = np.zeros((k,d))
@@ -95,28 +107,23 @@ def buildPhi(n, k, d):
 
 ### --- End of FJLT --- ###
 
+def evaluateKMeans(data, k, labels):
+    n = len(data)
 
-def evalKMeans(array,centers): #average of points to centers
-    total = 0
-    for i in range(len(array)):
-        best = -1
-        for j in range(len(centers)):
-            dist = 0
-            for k in range(len(centers[j])):
-                dist += abs(array[i][k]-centers[j][k])
-            if dist<best or best ==-1:
-                best = dist
-        total += best
-    return total/len(array)
-
-def computeKMeans(array, k, orig_data=digits):
     start = time.time()
     kmeans = KMeans(n_clusters=k
         # , random_state=4
-        ).fit(array)
+        ).fit(data)
     end = time.time()
-    print("KMeans took "+str(end-start) +" secs.")
-    print("KMeans avg. error: "+str(evalKMeans(array, kmeans.cluster_centers_)))
+
+    predictions = kmeans.predict(data)
+
+    score = sum([1 for i,j in zip(predictions, labels) if i == j])
+
+    err = float(n-score)/n
+
+    print("KMeans took %f secs." % (end-start))
+    print("KMeans error: %f" % err)
     #print(kmeans.labels_)
     #print(kmeans.cluster_centers_)
 
@@ -124,20 +131,20 @@ def computeKMeans(array, k, orig_data=digits):
     for center in kmeans.cluster_centers_:
         #try to see what they correspond to
         best = (-1,np.inf)
-        for i in range(len(array)):
-            #l = matToList(im)
-            l = array[i]
+        for i in range(len(data)):
+            #l = linearizeMatrix(im)
+            l = data[i]
             dist = 0
             for j in range(len(l)):
                 dist += abs(center[j]-l[j])
             if dist < best[1]:
                 best = (i,dist)
-        printDigit(digits.images[best[0]])
+        printDigit(orig_data[best[0]])
     """
 
 def printDigits(digits):
     for i in range(1, len(digits)):
-        printDigit(digits.images[i])
+        printDigit(digits[i])
 
 def printDigit(digit):
     plt.imshow(digit, cmap='gray')
