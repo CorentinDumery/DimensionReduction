ab3e6b12a176a059d45f682ff02b565132e22150
CorentinDumery <43251496+CorentinDumery@users.noreply.github.com>
2019-10-24 16:53:17 +0800

    Add estimation for coin JLT

--- JLT_methods.py
+++ JLT_methods.py
@@ -12,12 +12,29 @@ import time
 # question 2 if I remember correctly we don't need to prove stuff
 
 def Achlioptas_phi(n, d, k):
-
-    # eps = 0.99
-    # beta = 0.01
-    # k0 = log(n)*(4+2*beta)/(eps*eps/2 - (eps**3)/3)
-    # k = int(k0)+1
-    # ... (1/sqrt(k))*rowList.dot(R)
+    #Estimation of the transforms accuracy :
+    #beta is set such that we get "good" results with a >= 2/3 probability
+    #then epsilon is deduced from the rest and defines how "good" it actually is
+    beta = log(3)/log(n) 
+    
+    x0 = 0
+    x1 = 1
+    objective = log(n)*(4+2*beta)/k
+    if (objective>0.1666):
+        print("\t\tWarning : output's dimension is too low, the proof doesn't hold.")
+    else:
+        precision = 0.001
+        while True:
+            x = (x0+x1)/2
+            fx = (x**2)/2 - (x**3)/3
+            if fx<objective-precision:
+                x0 = (x0+x1)/2
+            elif fx>objective+precision:
+                x1 = (x0+x1)/2
+            else:
+                break
+        eps = x
+        print("\t\tEpsilon factor of the approximation: %f" % (eps))
 
     non_zero = (d/3)**(-1/2)
 
