a417ccda60a93ecfc217cf417cbaccf3de0acab7
CapelliDiPaja <24519853+FireSterLine@users.noreply.github.com>
2019-11-02 22:25:11 +0800

    Improved main. Add few test problems. KNeighbors is cool

--- main.py
+++ main.py
@@ -30,27 +30,29 @@ def warn(*args, **kwargs):
 #########################################################################
 #########################################################################
 
+CleanOutput = True
 # Number of repeated runs
 R = 3
 
-# data, labels, n_clusters = loadDigits()
-data, labels, n_clusters = loadMNIST()
-# data, labels, n_clusters = loadTaxis()
-# data, labels, n_clusters = loadHighD_1024()
+load_dataset_fun = loadDigits
+# load_dataset_fun = loadMNIST
+# load_dataset_fun = loadHighD_1024
 
 # TODO: real-world datasets
+# load_dataset_fun = loadTaxis
 #  http://archive.ics.uci.edu/ml/datasets/Gisette
 #  http://archive.ics.uci.edu/ml/datasets/Dorothea
 #  http://archive.ics.uci.edu/ml/datasets/Dexter
 
-# methods = [Achliop, FastJLT, sampDim]
 methods = [Achliop, FastJLT, sampDim, HVarDim, useLDA, usePCA, FactAna]
+# methods = [Achliop, FastJLT, sampDim]
 
-DontTestAgainstGT = True
-CleanOutput = True
+# One might not want to eval against the ground truth
+#  because the results against the baseline are sufficient (and very similar)
+DontEvalAgainstGT = True
 
+test_problems = [kneigh_, kmeans_] # spectr_
 # test_problems = [kmeans_]
-test_problems = [kmeans_, mshift_]
 # test_problems = [mshift_]
 
 # dim_red_factor = [0.5, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
@@ -69,28 +71,34 @@ dim_red_factor = [0.5, 0.2, 0.1, 0.01, 0.001, 0.0001]
 def main():
 
     print()
-    print("#######################################")
-    print("### Johnson-Lindenstrauss Transform ###")
-    print("#######################################")
+    print("###################################################")
+    print("### Dimensionality Reduction Methods Comparison ###")
+    print("###################################################")
     print()
 
     np.random.seed(0)
 
     methods_names = {
         "Achliop"     : "Achlioptas' coin flip method",
-        "FastJLT"        : "Fast JL Transform method",
+        "FastJLT"     : "Fast Johnson-Lindenstrauss Transform method",
         "sampDim"     : "Simple dimensions sampling",
-        "HVarDim"    : "Select high-variance dimensions",
-        "useLDA"         : "LDA",
-        "usePCA"         : "PCA",
-        "FactAna"      : "FactorAnalysis",
+        "HVarDim"     : "Select high-variance dimensions",
+        "useLDA"      : "LDA",
+        "usePCA"      : "PCA",
+        "FactAna"     : "FactorAnalysis",
     }
 
+
+    data, labels, n_clusters = load_dataset_fun()
     n, d = data.shape
 
     print("n * d = %d * %d" % (n, d))
     print("n_clusters = %d" % n_clusters)
     print("R = %d" % (R))
+
+    if CleanOutput:
+        print("n\td\tn_clust\ttest_pr\teval_against\tmethod\tk\tn/k\tvmeas\tvmeas_e\ttime")
+        # E.g 10000	784	mshift_	baseline	Achliop	392	0.5	0.000 +/- 0.00	14.589523
 
     # For each method
     for method_fun in methods:
@@ -108,16 +116,17 @@ def main():
             if not CleanOutput:
                 print("\tPure " + test_problem.__doc__ + "...")
 
-            test_against = [{'name' : "gr-truth", 'labs' : labels}]
+            eval_against = [{'name' : "gr-truth", 'labs' : labels}]
             if labels is None:
-                test_against = []
-            naive_labels = evaluate(data, test_problem, n_clusters, test_against, R=R)
+                eval_against = []
+            naive_labels = evaluate(data, test_problem, n_clusters, eval_against, R=R)
 
             # For some dim. red
-            for kd in dim_red_factor:
-                k = int(kd * d)
-                if k > d or k < 1:
-                    continue
+            ks = np.unique((np.array(dim_red_factor)*100).astype(int).clip(1, d))
+            ks[::-1].sort()
+            for k in ks:
+                kd = k/n
+
                 if method_fun == useLDA and k > min(d, len(np.unique(n_clusters))-1):
                     continue
 
@@ -126,16 +135,16 @@ def main():
                     print("\t\tDim. reduction: %d -> %d" % (d, k))
 
 
-                test_against = [{'name' : "gr-truth", 'labs' : labels}, {'name' : "baseline", 'labs' : naive_labels}]
-                if labels is None or DontTestAgainstGT:
-                    test_against = [{'name' : "baseline", 'labs' : naive_labels}]
+                eval_against = [{'name' : "gr-truth", 'labs' : labels}, {'name' : "baseline", 'labs' : naive_labels}]
+                if labels is None or DontEvalAgainstGT:
+                    eval_against = [{'name' : "baseline", 'labs' : naive_labels}]
 
-                evaluate(data, test_problem, n_clusters, test_against, R=R, reduce_by=(method_fun, k))
+                evaluate(data, test_problem, n_clusters, eval_against, reduce_by=(method_fun, k), R=R)
 
     print()
 
 # Use KMeans as clustering method for comparison
-def evaluate(data, test_problem, n_clusters, base_labels_arrs, R=2, reduce_by=None):
+def evaluate(data, test_problem, n_clusters, base_labels_arrs, reduce_by=None, R=2):
     n, d = data.shape
     b = len(base_labels_arrs)
 
@@ -151,7 +160,7 @@ def evaluate(data, test_problem, n_clusters, base_labels_arrs, R=2, reduce_by=No
         k_name = str(k)
         kd_name = "%.5f" % (k/d)
 
-    # For each labels to test against
+    # For each labels to evaluate against
     for i,base in enumerate(base_labels_arrs):
         labels = base['labs']
 
@@ -166,14 +175,17 @@ def evaluate(data, test_problem, n_clusters, base_labels_arrs, R=2, reduce_by=No
             if reduce_by != None:
                 reduced_data = method_fun(data, k, labels, silent=CleanOutput)
                 if reduced_data.shape[1] != k:
-                    ValueError("Error! reduction failed:")
                     print(data.shape, reduced_data.shape)
+                    raise ValueError("Error! reduction failed:")
             else:
                 reduced_data = data
 
             time_elapsed_avg -= time.time()
-            clust = test_problem(reduced_data, n_clusters)
+            clust = test_problem(reduced_data, n_clusters, labels)
             time_elapsed_avg += time.time()
+
+            if len(np.unique(clust)) != n_clusters:
+                print("Error! Check the code. The clustering routine only used %d of the %d available cluster(s)." % (len(np.unique(clust)), n_clusters))
 
             v_measure = metrics.v_measure_score(labels, clust)
             v_measure_avg += v_measure
@@ -188,9 +200,10 @@ def evaluate(data, test_problem, n_clusters, base_labels_arrs, R=2, reduce_by=No
             print("\t\tv-measure/%s:\t%0.3f +/- %0.2f" % (base['name'], v_measure_avg, (v_measure_max-v_measure_min)/2/v_measure_avg))
             print("\t\tAvg. time: %f secs." % (time_elapsed_avg))
         else:
-            print("%d\t%d\t%s\t%s\t%s\t%s\t%s\t%0.3f +/- %0.2f\t%f" % (
+            print("%d\t%d\t%d\t%s\t%s\t%s\t%s\t%s\t%0.3f\t%0.2f\t%f" % (
                 n,
                 d,
+                n_clusters,
                 test_problem.__name__,
                 base['name'],
                 method_name,
--- test_problems.py
+++ test_problems.py
@@ -1,26 +1,86 @@
 import numpy as np
 # from sklearn.svm import SVC
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import MeanShift, estimate_bandwidth
+from scipy.spatial.distance import cdist
 
-def kmeans_(data, k):
+def kmeans_(data, n_clusters, y):
     """K-means"""
     kmeans = KMeans(
-        n_clusters=k
+        n_clusters=n_clusters
         # , random_state=4
         ).fit(data)
     return kmeans.labels_
 
-def mshift_(data, k):
+def spectr_(data, n_clusters, y):
+    """Spectral Clustering"""
+    sc = SpectralClustering(
+        n_clusters=n_clusters,
+        assign_labels="discretize",
+        ).fit(data)
+    return sc.labels_
+
+def kneigh_(data, n_clusters, y):
+    """KNN"""
+    k=max(10, int(data.shape[0]/n_clusters/2))
+    neigh = KNeighborsClassifier(n_neighbors=k)
+    neigh.fit(data, y)
+    return neigh.predict(data)
+
+def mshift_(data, n_clusters, y):
     """MeanShift"""
     # The following bandwidth can be automatically detected using
     # Check parameters ...
-    bandwidth = estimate_bandwidth(data, quantile=0.2, n_samples=500)
+    n,d = data.shape
+    # bandwidth = estimate_bandwidth(data, quantile=0.2, n_samples=10)
 
-    n,k = data.shape
     ms = MeanShift(
-        bandwidth=2.,
-        seeds=data[np.random.randint(n, size=k)],
-        bin_seeding=True
+        bandwidth=1.,
+        seeds=data[np.random.randint(n, size=int(n/10))],
+        bin_seeding=False
     ).fit(data)
-    return ms.labels_
+
+    cluster_centers = ms.cluster_centers_
+    labels = ms.labels_
+
+    return labels
+
+    # Merge cluster_centers until you have the desired number
+    while len(cluster_centers) > n_clusters:
+        # print(cluster_centers.shape)
+        dists = cdist(cluster_centers, cluster_centers, 'euclidean')
+        min_dists = dists.argsort(axis=1)[:,1]
+
+        min_i = 0
+        for i,min_j in enumerate(min_dists):
+            if dists[i,min_j] < dists[min_i,min_dists[min_i]]:
+                min_i = i
+        # Min: dists[min_i,min_dists[min_i]]
+        # join clusters min_i and min_dists[min_i]
+        min_j = min_dists[min_i]
+
+        # print()
+        # print(len(cluster_centers))
+        # print(len(np.unique(labels)))
+        # print(labels.shape, data.shape, cluster_centers.shape)
+        # print("merging")
+        # print(min_i, min_j)
+        # print(labels[np.any([labels == min_i, labels == min_j], axis = 0)])
+        new_center = data[np.any([labels == min_i, labels == min_j], axis = 0)].mean(axis=0)
+
+        cluster_centers[min_i] = new_center
+        labels[labels == min_j] = min_i
+
+        cluster_centers = np.delete(cluster_centers, min_j,0)
+        for j in range(min_j, len(cluster_centers)):
+            labels[labels == j+1] = j
+
+
+        if not len(cluster_centers) == len(np.unique(labels)):
+            raise ValueError("ciao")
+
+
+    # print(len(np.unique(labels)))
+    # print(len(cluster_centers))
+    return labels
